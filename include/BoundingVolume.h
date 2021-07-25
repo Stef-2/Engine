7e62c5d6900f7dc03b4ae422bd5fb68897f0ee4f
Stef-2 <capteinobvious@gmail.com>
2021-07-25 19:13:33 +0200

    Further work on OcTrees.

--- include/BoundingVolume.h
+++ include/BoundingVolume.h
@@ -3,18 +3,21 @@
 
 #include "glm/glm.hpp"
 
+#include "memory"
+
 namespace Engine
 {
+    //forward declaration of BoudingSphere so BoundingBox can reference it
     struct BoundingSphere;
 
     //axis aligned bounding box, fully encloses a mesh
     //we're defining a bounding box using two diagonal corners
-    //           o-------o <-second
+    //           o-------o <-maxs
     //          /|      /|
     //         o-+-----o |
     //         | o-----+-o
     //         |/      |/
-    // first-> o-------o
+    //  mins-> o-------o
     struct BoundingBox
     {
         //intersection checks
@@ -24,17 +27,17 @@ namespace Engine
         void Move(glm::vec3 offset);
         void Move(glm::vec3 direction, float magnitude);
 
-        inline float GetBottom();
-        inline float GetLeft();
-        inline float GetFront();
-        inline float GetTop();
-        inline float GetRight();
-        inline float GetBack();
+        float GetBottom();
+        float GetLeft();
+        float GetFront();
+        float GetTop();
+        float GetRight();
+        float GetBack();
 
         //bottom, left, front
-        glm::vec3 first;
+        glm::vec3 mins;
         //top, right, back
-        glm::vec3 second;
+        glm::vec3 maxs;
     };
 
     //bounding sphere, fully encloses a mesh
@@ -49,6 +52,39 @@ namespace Engine
 
         glm::vec3 center;
         float radius;
+    };
+
+    //a struct meant to be used as a node in the ocTree
+    //holds information about its relatives and enclosing data in additon to the inherited position info
+    template<typename T>
+    struct BoundingNode : Engine::BoundingBox
+    {
+        BoundingNode();
+        ~BoundingNode();
+        BoundingNode(glm::vec3 mins, glm::vec3 maxs);
+
+        void Subdivide(int maxDepth);
+        
+        BoundingBox* parent;
+        BoundingBox* siblings;
+        BoundingBox* children;
+
+        unsigned int depth;
+        bool isLeaf;
+        T* data;
+    };
+
+    //Octree class, used to recursively subdivide 3D space into 8 equally sized BoundingBoxes (BoundingNodes)
+    template<typename T>
+    class OcTree
+    {
+    public:
+        OcTree();
+        OcTree(int depth);
+        void Subdivide(int maxDepth);
+
+        BoundingNode<T> child;
+        unsigned int depth;
     };
 }
 #endif // VERTEXBUFFER_H
--- src/BoundingVolume.cpp
+++ src/BoundingVolume.cpp
@@ -2,14 +2,14 @@
 
 void Engine::BoundingBox::Move(glm::vec3 offset)
 {
-    this->first += offset;
-    this->second += offset;
+    this->mins += offset;
+    this->maxs += offset;
 }
 
 void Engine::BoundingBox::Move(glm::vec3 direction, float magnitude)
 {
-    this->first += glm::normalize(direction) * magnitude;
-    this->second += glm::normalize(direction) * magnitude;
+    this->mins += glm::normalize(direction) * magnitude;
+    this->maxs += glm::normalize(direction) * magnitude;
 }
 
 void Engine::BoundingSphere::Move(glm::vec3 offset)
@@ -58,25 +58,25 @@ bool Engine::BoundingBox::IntersectsWith(Engine::BoundingSphere& sphere)
     float distance = glm::pow(sphere.radius, 2);
 
     //x axis
-    if (sphere.center.x < this->first.x)
-        distance -= glm::pow(sphere.center.x - this->first.x, 2);
+    if (sphere.center.x < this->mins.x)
+        distance -= glm::pow(sphere.center.x - this->mins.x, 2);
     else
-        if (sphere.center.x > this->second.x)
-            distance -= glm::pow(sphere.center.x - this->second.x, 2);
+        if (sphere.center.x > this->maxs.x)
+            distance -= glm::pow(sphere.center.x - this->maxs.x, 2);
 
     //y axis
-    if (sphere.center.y < this->first.y)
-        distance -= glm::pow(sphere.center.y - this->first.y, 2);
+    if (sphere.center.y < this->mins.y)
+        distance -= glm::pow(sphere.center.y - this->mins.y, 2);
     else
-        if (sphere.center.y > this->second.y)
-            distance -= glm::pow(sphere.center.y - this->second.y, 2);
+        if (sphere.center.y > this->maxs.y)
+            distance -= glm::pow(sphere.center.y - this->maxs.y, 2);
 
     //z axis
-    if (sphere.center.z < this->first.z)
-        distance -= glm::pow(sphere.center.z - this->first.z, 2);
+    if (sphere.center.z < this->mins.z)
+        distance -= glm::pow(sphere.center.z - this->mins.z, 2);
     else
-        if (sphere.center.z > this->second.z)
-            distance -= glm::pow(sphere.center.z - this->second.z, 2);
+        if (sphere.center.z > this->maxs.z)
+            distance -= glm::pow(sphere.center.z - this->maxs.z, 2);
 
     return distance > 0;
 }
@@ -89,36 +89,158 @@ bool Engine::BoundingSphere::IntersectsWith(Engine::BoundingSphere& other)
 
 bool Engine::BoundingSphere::IntersectsWith(Engine::BoundingBox& bBox)
 {
-    //just reuse the algorithm from the bounding boxes perspective
+    //just reuse the intersection algorithm from the bounding boxes perspective
     return bBox.IntersectsWith(*this);
 }
 
-inline float Engine::BoundingBox::GetBottom() {
+float Engine::BoundingBox::GetBottom() {
 
-    return this->first.y;
+    return this->mins.y;
 }
 
-inline float Engine::BoundingBox::GetLeft() {
+float Engine::BoundingBox::GetLeft() {
 
-    return this->first.x;
+    return this->mins.x;
 }
 
-inline float Engine::BoundingBox::GetFront() {
+float Engine::BoundingBox::GetFront() {
 
-    return this->first.z;
+    return this->mins.z;
 }
 
-inline float Engine::BoundingBox::GetTop() {
+float Engine::BoundingBox::GetTop() {
 
-    return this->second.y;
+    return this->maxs.y;
 }
 
-inline float Engine::BoundingBox::GetRight() {
+float Engine::BoundingBox::GetRight() {
 
-    return this->second.x;
+    return this->maxs.x;
 }
 
-inline float Engine::BoundingBox::GetBack() {
+float Engine::BoundingBox::GetBack() {
 
-    return this->second.z;
+    return this->maxs.z;
+}
+
+template<typename T>
+Engine::BoundingNode<T>::BoundingNode()
+{
+    this->mins = {};
+    this->maxs = {};
+
+    this->depth = {};
+    this->isLeaf = {};
+    this->parent = {};
+    this->siblings = {};
+    this->children = {};
+    this->data = {};
+}
+
+template<typename T>
+Engine::BoundingNode<T>::~BoundingNode()
+{
+    if (this->parent) delete parent;
+    if (this->siblings) delete siblings;
+    if (this->children) delete children;
+    if (this->data) delete data;
+}
+
+template<typename T>
+Engine::BoundingNode<T>::BoundingNode(glm::vec3 mins, glm::vec3 maxs)
+{
+    this->mins = mins;
+    this->maxs = maxs;
+
+    this->depth = {};
+    this->isLeaf = {};
+    this->parent = {};
+    this->siblings = {};
+    this->children = {};
+    this->data = {};
+}
+
+template<typename T>
+void Engine::BoundingNode<T>::Subdivide(int maxDepth)
+{
+    //check if we've reached maximum depth
+    if (this->depth != maxDepth)
+    {
+        //if not, spawn 8 children and raise them well
+        this->children = new BoundingNode<T>[8];
+
+        glm::vec3 halfSize = (this->maxs - this->mins) / 2;
+
+        //setup the child coordinates relative to the proud parent's one
+        //this probably could have been done in some black magic loop but I had too many coffees already
+        //starting at this->mins location and going in clockwise direction
+
+        //bottom near left
+        this->children[0].mins = glm::vec3(this->mins);
+
+        //bottom far left
+        this->children[1].mins = glm::vec3(this->mins + halfSize.z);
+
+        //bottom far right
+        this->children[2].mins = glm::vec3(this->mins + halfSize.z + halfSize.x);
+
+        //bottom near right
+        this->children[3].mins = glm::vec3(this->mins + halfSize.x);
+
+        //top near left
+        this->children[4].mins = glm::vec3(this->mins + halfSize.y);
+
+        //top far left
+        this->children[5].mins = glm::vec3(this->mins + halfSize.z + halfSize.y);
+
+        //top far right
+        this->children[6].mins = glm::vec3(this->mins + halfSize);
+
+        //top near right
+        this->children[7].mins = glm::vec3(this->mins + halfSize.x + halfSize.y);
+
+        //setup other children parameters
+        for (size_t i = 0; i < 8; i++)
+        {
+            this->children[i].depth = this->depth - 1;
+            this->children[i].isLeaf = false;
+            this->children[i].parent = this;
+            this->children[i].maxs = this->children[i].mins + halfSize;
+            this->children[i].siblings = new BoundingNode<T>[7];
+
+            //get siblings working
+            for (size_t j = 0; j < 8; j++)
+                if (this->children[i] != this->children[j])
+                    this->children[i].siblings[j] = this->children[j];
+        }
+
+        //they grow up so fast, now they get to have children of their own
+        for (size_t i = 0; i < 8; i++)
+            this->children[i].Subdivide(this->depth);
+    }
+    else
+    {
+        this->isLeaf = true;
+        this->children = {};
+    }
+}
+
+template<typename T>
+Engine::OcTree<T>::OcTree()
+{
+    this->child = {};
+    this->depth = {};
+}
+
+template<typename T>
+Engine::OcTree<T>::OcTree(int maxDepth)
+{
+    this->child = {};
+    this->depth = maxDepth;
+}
+
+template<typename T>
+void Engine::OcTree<T>::Subdivide(int maxDepth)
+{
+    this->child->Subdivide(maxDepth);
 }
